7fcb4485b50ae7b31338df34a7025b20df60f150
Romano Pagnan <romano.pag@gmail.com>
2016-12-29 18:02:06 +0100

    Added Geolocation and request from OpenWeather.

--- weather/app.js
+++ weather/app.js
@@ -1,1 +1,29 @@
+const coord = {
+  lat: 0,
+  lon: 0
+};
+let weather; // data from OpenWeather
 
+const getWeather = () => {
+  // parameters for JSONP request
+  const url = 'http://api.openweathermap.org/data/2.5/weather?';
+  const data = {
+    APPID: 'd931aceea21eb4832329f45673a512cf',
+    lat: coord.lat,
+    lon: coord.lon
+  };
+  const callback = (res) => {
+    weather = res;
+    console.log(weather);
+  };
+
+  $.getJSON(url, data, callback);
+} // end getWeather
+
+
+//get current coordinates
+navigator.geolocation.getCurrentPosition(function(position) {
+  coord.lat = position.coords.latitude;
+  coord.lon = position.coords.longitude;
+  getWeather();
+});
