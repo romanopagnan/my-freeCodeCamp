be1e3eb8c616f1b6816513a1ee44097ab617f7e5
Romano Pagnan <romano.pag@gmail.com>
2016-12-30 19:19:50 +0100

    Added data from OpenWeather and F°/C° toggle to index.html

--- weather/app.js
+++ weather/app.js
@@ -2,22 +2,30 @@ const coord = {
   lat: 0,
   lon: 0
 };
-let weather; // data from OpenWeather
+let weather; // Object from OpenWeather
 
 const getWeather = () => {
-  // parameters for JSONP request
-  const url = 'http://api.openweathermap.org/data/2.5/weather?';
-  const data = {
+  $.getJSON('http://api.openweathermap.org/data/2.5/weather?',{
     APPID: 'd931aceea21eb4832329f45673a512cf',
     lat: coord.lat,
-    lon: coord.lon
-  };
-  const callback = (res) => {
+    lon: coord.lon,
+    units: 'imperial'
+    }, // end data
+    (res) => { // CALLBACK
     weather = res;
-    console.log(weather);
-  };
+    console.log(weather); // dev
 
-  $.getJSON(url, data, callback);
+    $('.weather').text(weather.weather[0].main);
+    $('.location').text(weather.name);
+    $('.temperature').text(weather.main.temp + '° F');
+    $('.toggle-temp').click(() => {
+      const temp = $('.temperature').text();
+      if  (temp.charAt(temp.length - 1) == 'F')
+        $('.temperature').text(((weather.main.temp - 32) / 1.8).toFixed(2) + '° C');
+      else
+        $('.temperature').text(weather.main.temp + '° F');
+    }) //end click
+  }); //end getJSON
 } // end getWeather
 
 
@@ -26,4 +34,4 @@ navigator.geolocation.getCurrentPosition(function(position) {
   coord.lat = position.coords.latitude;
   coord.lon = position.coords.longitude;
   getWeather();
-});
+}); // end navigator
